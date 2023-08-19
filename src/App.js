4e867f6053eee8c1cbe11e7a039bd25c154b13f6
lokesh_krishna_cherukuri <lci2022015@iiitl.ac.in>
2023-08-19 13:30:36 +0530

    formatted all documents

--- src/App.js
+++ src/App.js
@@ -1,88 +1,139 @@
-import './App.css'
-import Signup from './components/Signup/Signup';
-import  { useAuth } from './Context/AuthContext';
-import { Route, Routes } from 'react-router-dom';
-import LoadingBar from 'react-top-loading-bar';
+import "./App.css";
+import Signup from "./components/Signup/Signup";
+import { useAuth } from "./Context/AuthContext";
+import { Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-import Login from './components/Login/Login';
+import Login from "./components/Login/Login";
 
-import Loading from './components/Loading Page/Loading';
-import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import Loading from "./components/Loading Page/Loading";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 
-import News from './components/News'
-import { useState } from 'react';
-
-
+import News from "./components/News";
+import { useState } from "react";
 
 function App() {
   const { load, currentUser } = useAuth();
-  
-  const [progress,setprogress]=useState(1)
 
+  const [progress, setprogress] = useState(1);
 
-  const apikey= "cb9e2473d9694172b045bd1157bd6e9e"
-
+  const apikey = "cb9e2473d9694172b045bd1157bd6e9e";
 
   return (
+    <div className="div">
+      {load ? (
+        <Loading />
+      ) : (
+        <>
+          {!currentUser ? (
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          ) : (
+            <>
+              <LoadingBar color="#f11946" progress={progress} height={3} />
 
-      
-        <div className='div' >
-
-
-
-
-          {
-            load ?(
-              <Loading/>
-            ):
-            (
-
-              <>
-              {
-                !currentUser?(
-                  <Routes>
-                    <Route exact path='/' element={<Login/>}/>
-                    <Route path="/signup" element={<Signup/>} />
-                    <Route path="forgot-password" element={<ForgotPassword/>}/>
-                  </Routes>
-
-                ):(
-                  <>
-<LoadingBar
-        color='#f11946'
-        progress={progress}
-        height={3}
-      />
-                  
-                    <Routes>
-                      <Route  exact path='/dashboard' element={<News setProgress={(x)=>setprogress(x)} apikey={apikey}    key='general'  country="in" category="general" />}/> 
-                      <Route  exact path='/dashboard/business' element={<News  setProgress={(x)=>setprogress(x)} apikey={apikey}  key='busines'  country="in" category="business" />}/>
-                      <Route  exact path='/dashboard/entertainment' element={<News setProgress={(x)=>setprogress(x)} apikey={apikey}   key='entertainment' country="in" category="entertainment" />}/>
-                      <Route  exact path='/dashboard/health' element={<News  setProgress={(x)=>setprogress(x)} apikey={apikey}  key='health'  country="in" category="health" />}/>
-                      <Route  exact path='/dashboard/science' element={<News setProgress={(x)=>setprogress(x)} apikey={apikey}   key='science'  country="in" category="science" />}/>
-                      <Route  exact path='/dashboard/sports' element={<News  setProgress={(x)=>setprogress(x)} apikey={apikey}  key='sports'  country="in" category="sports" />}/>
-                      <Route  exact path='/dashboard/technology' element={<News setProgress={(x)=>setprogress(x)} apikey={apikey}   key='technology'  country="in" category="technology" />}/>
-                      <Route path='/' element={<Login/>}/>
-                    </Routes>
-                  
-                  
-                    </>
-                )
-
-                
-              }
-              </>
-
-              
-            )
-          }
-
-          
-        
-        </div>      
-
-    
-
+              <Routes>
+                <Route
+                  exact
+                  path="/dashboard"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="general"
+                      country="in"
+                      category="general"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/business"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="busines"
+                      country="in"
+                      category="business"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/entertainment"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="entertainment"
+                      country="in"
+                      category="entertainment"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/health"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="health"
+                      country="in"
+                      category="health"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/science"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="science"
+                      country="in"
+                      category="science"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/sports"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="sports"
+                      country="in"
+                      category="sports"
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/dashboard/technology"
+                  element={
+                    <News
+                      setProgress={(x) => setprogress(x)}
+                      apikey={apikey}
+                      key="technology"
+                      country="in"
+                      category="technology"
+                    />
+                  }
+                />
+                <Route path="/" element={<Login />} />
+              </Routes>
+            </>
+          )}
+        </>
+      )}
+    </div>
   );
 }
 
